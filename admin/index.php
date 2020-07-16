86291dd53931d0be70e0a8afba77224a4e1fe9e2
Hritikkumar <kumarhritik38@gmail.com>
2020-07-16 16:21:29 +0530

    Admin login page interface updated.

--- admin/index.php
+++ admin/index.php
@@ -44,16 +44,17 @@ else{
         <!-- Styles -->
         <link type="text/css" rel="stylesheet" href="../assets/plugins/materialize/css/materialize.min.css"/>
         <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
         <link href="../assets/plugins/material-preloader/css/materialPreloader.min.css" rel="stylesheet">        
         <link href="../assets/css/alpha.min.css" rel="stylesheet" type="text/css"/>
         <link href="../assets/css/custom.css" rel="stylesheet" type="text/css"/>
     </head>
-    <body class="signin-page">
+    <!-- <body class="signin-page">
 
         <div class="mn-content valign-wrapper">
 
             <main class="mn-inner container">
-  <h4 align="center"><a href="../index.php">Leave Management System | Admin Login</a></h4>
+            <h4 align="center"><a href="../index.php">Leave Management System | Admin Login</a></h4>
                 <div class="valign">
                       <div class="row">
 
@@ -83,9 +84,52 @@ else{
                     </div>
                 </div>
             </main>
+        </div> -->
+        <body class="admin-signin">
+        <div class="container">
+        <div class="row text-center">
+            <div class="col">
+            <h1><a href="../index.php">Leave Management System | Admin Login</a></h1>
+            </div>
+            </div>
         </div>
-        
+        <div class="container w-75 ">
+                    <!-- <main class="mn-inner container"> -->
+            
+                      <div class="row shadow-lg rounded bg-white p-4">
+                            <div class="col-4 p-0">
+                            <img src="../assets/images/admin.jpg" alt="" class="img-fluid admin-img rounded">
+                            </div>
+                          <div class="col">
+                              <div class="card" style="height:100%; box-shadow: none; border:none;">
+                                  <div class="card-content ">
+                                      <span class="card-title">Sign In</span>
+                                       <div class="row">
+                                           <form class="col s12" name="signin" method="post">
+                                               <div class="input-field col s12">
+                                                   <input id="username" type="text" name="username" class="validate" autocomplete="off" required >
+                                                   <label for="email">Username</label>
+                                               </div>
+                                               <div class="input-field col s12">
+                                                   <input id="password" type="password" class="validate" name="password" autocomplete="off" required>
+                                                   <label for="password">Password</label>
+                                               </div>
+                                               <div class="col s12 right-align m-t-sm">
+                                                
+                                                   <input type="submit" name="signin" value="Sign in" class="waves-effect waves-light btn teal">
+                                               </div>
+                                           </form>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                </div>
+            <!-- </main> -->
+        </div>
         <!-- Javascripts -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
         <script src="../assets/plugins/jquery/jquery-2.2.0.min.js"></script>
         <script src="../assets/plugins/materialize/js/materialize.min.js"></script>
         <script src="../assets/plugins/material-preloader/js/materialPreloader.min.js"></script>
